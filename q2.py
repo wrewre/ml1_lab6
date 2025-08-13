0b7d5a3d87e49eeb47d6a05c54594432922fd1e4
wrewre <105921183+wrewre@users.noreply.github.com>
2025-08-13 12:24:14 +0530

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import math as mp
+df=pd.read_csv(r"C:\Users\prana\OneDrive\Desktop\machine_learning\lab6\DCT_mal.csv")
+values=df['LABEL'].value_counts().to_dict()
+total=0
+length=len(values)
+summation=sum(values.values())
+print(length)
+dictionary={}
+for i in values:
+    dictionary[i]=values[i]/summation
+print(dictionary)
+
+for j in dictionary:
+    total+=(dictionary[j]*dictionary[j])
+print((1-total))
